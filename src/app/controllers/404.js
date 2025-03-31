dde89115a8630038a581bb1453c4dace44da1d6e
khanh251204 <hoangkhanh281004@gmail.com>
2025-03-31 16:24:03 +0700

    .gitignore

--- src/app/controllers/404.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/User');
-class M404Controller {
-    async m404(req, res) {
-        const us = req.session.userInfo;
-        return res.render('UI/404', {User:us});
-    }
-}
-
-module.exports = new M404Controller();
